ae3cc4c4c8834b70525d34b9e62179c8bb8a2bcf
Eddy Davila <EdDavila@fullsail.edu>
2013-04-18 15:50:19 -0400

    fixed errors

--- davila_eddy_project2/js/eddy_davila_p2.js
+++ davila_eddy_project2/js/eddy_davila_p2.js
@@ -2,4 +2,16 @@
 
 
 var textInput = document.getElementById("client");
+    var focusBorder = function (){
+        textInput.setAttribute("class", "addFocus");
+    }
+    
+var textInput = document.getElementById("client");
+    var blurBorder = function (){
+        textInput.removeAttribute("class", "addFocus");
+    }
+    
+textInput.addEventListener("focus" , focusBorder );
+textInput.addEventListener("blur" , blurBorder ); 
+
 console.log(textInput);
