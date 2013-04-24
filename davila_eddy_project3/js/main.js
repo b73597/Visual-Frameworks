8b4957843e76e2bd492615415cc9ee01bd949063
Eddy Davila <EdDavila@fullsail.edu>
2013-04-24 15:30:41 -0400

    Project 3

--- davila_eddy_project3/js/main.js
+++ davila_eddy_project3/js/main.js
@@ -131,7 +131,7 @@ function toggleControls(n){
 		item.comments			= ["Comments:", 	$('comments').value];
 	// Stringify
 	localStorage.setItem(id, JSON.stringify(item));
-	alert("Contact Saved!");
+	alert("Transaction Saved!");
 }
 
 	// Get Data
@@ -174,24 +174,79 @@ function makeItemLinks(key, linksLi) {
 	editLink.href = "#";
 	editLink.key = key;
 	var editText = "Edit Guitar Transaction";
-	//editLink.addEventListener("click", editItem);
+	editLink.addEventListener("click", editItem);
 	editLink.innerHTML = editText;
 	linksLi.appendChild(editLink);
-	
+
 	// add linebreak
 	var breakTag = document.createElement('br');
 	linksLi.appendChild(breakTag);
-	
+
 	//add delete single link
 	var deleteLink = document.createElement('a');
 	deleteLink.href = "#";
 	deleteLink.key = key;
 	var deleteText = "Delete Guitar Transaction";
-	//delete.addEventListener("click" , deleteItem);
+	deleteLink.addEventListener("click" , deleteItem);
 	deleteLink.innerHTML = deleteText;
 	linksLi.appendChild(deleteLink);
 }
-	// clear local data
+
+function editItem(){
+		// grab data from local storage
+		var value = localStorage.getItem(this.key);
+		var item = JSON.parse(value);
+
+		// show form
+		toggleControls("off");
+
+		// populate the form fields with current localStorage values.
+		$('fname').value = item.fname[1];
+		$('lname').value = item.lname[1];
+		$('email').value = item.email[1];
+		$('date').value = item.date[1];
+		$('guitar').value = item.guitar[1];
+		$('store').value = item.store[1];
+		$('rating').value = item.rating[1];
+		$('pickups').value = item.pickups[1];
+		$('strings').value = item.strings[1];
+		$('effects').value = item.effects[1];
+		var radios = document.forms[0].curency;
+		for(var i = 0; i<radios.length; i++){
+			if(radios.value == "Cash" && item.curency[1] == "Cash"){
+				radios[i].setAttribute("checked", "checked");
+			}else if(radios[i].value == "Credit" && item.curency[1] == "Credit"){
+				radios[i].setAttribute("checked", "checked");
+			}
+		}
+		if(item.coupon[1] == "Yes"){
+			$('coupon').setAttribute("checked", "checked");
+		}
+		$('comments').value = item.comments[1];
+
+		// remove the initial listener from the input save button
+		save.removeEventListener("click", storeData);
+
+		// change submit button value to edit button
+		$('submit').value = "Edit Guitar Transaction";
+		var editSubmit = $('submit');
+
+		//save key value
+		editSubmit.addEventListener("click", validate);
+		editSubmit.key = this.key;
+	}
+
+	function deleteItem(){
+		var ask = confirm("Are you sure you want to delete?");
+		if(ask){
+			localStorage.removeItem(this.key);
+			alert("Contact was deleted!");
+			window.location.reload();
+		}else{
+			alert("Contact was not deleted.");
+		}
+	}	
+	// clear local data ------------------------------------------
 
 function clearLocal(){
 	if(localStorage.length === 0){
@@ -203,6 +258,56 @@ function clearLocal(){
 		return false;
 	}
 }
+
+
+	function validate(e){
+		// what to validate
+		var getFname = $('fname');
+		var getLname = $('lname');
+		var getEmail = $('email');
+
+		// need input border 
+		errMsg.innerHTML = "";
+			getFname.style.border = "1px solid black";
+			getLname.style.border = "1px solid black";
+			getEmail.style.border = "1px solid black";
+
+		// error messages
+		var messageArry = [];
+		// first name validation
+		if(getFname.value === ""){
+			var fNameError = "Please enter your First Name."
+			getFname.style.border = "1px solid red";
+			messageArry.push(fNameError);
+		}
+		// last name validation
+		if(getLname.value === ""){
+			var lNameError = "Please enter your Last Name."
+			getLname.style.border = "1px solid red";
+			messageArry.push(lNameError);
+		}
+		// email validation
+		var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+		if(!(re.exec(getEmail.value))){
+			var emailError = "Please enter a valid email address.";
+			getEmail.style.border = "1px solid red";
+			messageArry.push(emailError);
+		}
+		if(messageArry.length >= 1){
+			for(var i =0, j=messageArry.length; i < j; i++){
+				var txt = document.createElement('li');
+				txt.innerHTML =messageArry[i];
+				errMsg.appendChild(txt);
+			}
+			e.preventDefault();
+			return false;
+		}else{
+			// save valid data
+			storeData(this.key);
+		}
+
+	}
+	
 
 	// Array for the drop down menu
 	var addon = ["--Pick Ups--", "EMG", "D Mustain", "Axis"],
